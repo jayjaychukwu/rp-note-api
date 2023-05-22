d4ba7ed70884bf86632b910719af6d83a3762715
jayjaychukwu <liljaydon.pj@gmail.com>
2023-05-22 22:20:23 +0100

    create and retrieve person

--- people.py
+++ people.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from flask import abort
+
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -26,3 +28,28 @@ PEOPLE = {
 
 def read_all():
     return list(PEOPLE.values())
+
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname", "")
+
+    if lname and lname not in PEOPLE:
+        PEOPLE[lname] = {"lname": lname, "fname": fname, "timestamp": get_timestamp()}
+        return PEOPLE[lname], 201
+
+    else:
+        abort(
+            406,
+            f"person with last name {lname} already exists",
+        )
+
+
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"person with last name {lname} not found",
+        )
