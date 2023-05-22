d3980f6ecf6ac9fb54b1aa4fcde188c7b8f649fd
jayjaychukwu <liljaydon.pj@gmail.com>
2023-05-23 00:45:07 +0100

    update and delete

--- people.py
+++ people.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import abort
+from flask import abort, make_response
 
 
 def get_timestamp():
@@ -53,3 +53,23 @@ def read_one(lname):
             404,
             f"person with last name {lname} not found",
         )
+
+
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"person with last name {lname} not found",
+        )
+
+
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(f"{lname} successfully deleted", 200)
+    else:
+        abort(404, f"person with last name {lname} not found")
